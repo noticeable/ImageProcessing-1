df1b2e568474e42d4bb74cfc1cb57c4627d730bd
佐々木拓馬 <Sasaki@TakumaMacBook-Air.local>
2017-02-17 23:11:08 +0900

    pgm画像とppm画像の保存

---
+++ saveimage.c
@@ -0,0 +1,35 @@
+// 画像データを保存する(ppm, pgm)
+#include "saveimage.h"
+
+void saveppm(char filename[], unsigned char *img, int width, int height)
+{
+    FILE *fp;
+    int band = 3;
+
+    if ((fp = fopen(filename, "wb")) == NULL)
+    {
+        printf("Not Open %s\n", filename);
+        exit(1);
+    }
+
+    fprintf(fp, "P6\n%d %d\n255\n", width, height);
+    fwrite(img, sizeof(unsigned char), width * height * band, fp);
+
+    fclose(fp);   
+}
+
+void savepgm(char filename[], unsigned char *img, int width, int height)
+{
+    FILE *fp;
+
+    if ((fp = fopen(filename, "wb")) == NULL)
+    {
+        printf("Not Open %s\n", filename);
+        exit(1);
+    }
+
+    fprintf(fp, "P5\n%d %d\n255\n", width, height);
+    fwrite(img, sizeof(unsigned char), width * height, fp);
+ 
+    fclose(fp);   
+}
--- saveimage.h
+++ saveimage.h
@@ -1,38 +1,7 @@
-// 画像データを保存する(ppm, pgm)
-#ifndef SAVEIMAGE_H
-#define SAVEIMAGE_H
+#ifndef _SAVEIMAGE_H_
+#define _SAVEIMAGE_H_
 
-void saveppm(char filename[], unsigned char *img, int width, int height)
-{
-    FILE *fp;
-    int band = 3;
-
-    if ((fp = fopen(filename, "wb")) == NULL)
-    {
-        printf("Not Open %s\n", filename);
-        exit(1);
-    }
-
-    fprintf(fp, "P6\n%d %d\n255\n", width, height);
-    fwrite(img, sizeof(unsigned char), width * height * band, fp);
-
-    fclose(fp);   
-}
-
-void savepgm(char filename[], unsigned char *img, int width, int height)
-{
-    FILE *fp;
-
-    if ((fp = fopen(filename, "wb")) == NULL)
-    {
-        printf("Not Open %s\n", filename);
-        exit(1);
-    }
-
-    fprintf(fp, "P5\n%d %d\n255\n", width, height);
-    fwrite(img, sizeof(unsigned char), width * height, fp);
- 
-    fclose(fp);   
-}
+void saveppm(char [], unsigned char *, int, int);
+void savepgm(char [], unsigned char *, int, int);
 
 #endif
