0e50f5282039f9c2f10d5e81bd9d1050aeb7d1bc
Takuma Sasaki <takuma@localhost.localdomain>
2017-02-20 20:50:21 +0900

    2次元kara射影変換

---
+++ perspectiveTransformation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "perspectiveTransformation.h"
+
+void perspectiveTrans2d(double A[], unsigned char *img, unsigned char *dst)
+{
+}
+
+void perspectiveTrans2d_two(double A_l[], unsigned char imgL*, unsigned char *dstL, double A_r[], unsigned char *imgR, unsigned char *dstR)
+{
+}
---
+++ perspectiveTransformation.h
@@ -0,0 +1,7 @@
+#ifndef PERSPECTIVETRANSFORMATION_H
+#define PERSPECTIVETRANSFORMATION_H
+
+void perspectiveTrans2d(double [], unsigned char *, unsigned char *);
+void perspectiveTrans2d_two(double [], unsigned char *, unsigned char *, double [], unsigned char *, unsigned char *);
+
+#endif
