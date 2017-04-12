c2a4e349aac4215f87118766c5d9ddf2dd120fbb
Sasaki <takuma@localhost.localdomain>
2017-04-12 20:13:41 +0900

    最適化

--- Calibration_3D_ver2/optimization_parameter.c
+++ Calibration_3D_ver2/optimization_parameter.c
@@ -8,6 +8,7 @@
 #define PI 3.141592
 
 void readcoeff(char [], double []);
+void readcoordinate(char [], double []);
 double fp(double []);
 double dfpa(double []); // 2点近似でalphaについて偏微分
 double dfpb(double []); // 2点近似でbetaについて偏微分
@@ -19,13 +20,13 @@ void update_x(double [], double); // ベクトルxの更新
 
 double coef_l[11];
 double coef_c[11];
+double coordinates[12] = {-37, 170, 364, 30, 180, 430, 30, 108.2, 430, -37, 100, 364}; // 70点だから210
 
 int main(void)
 {
     char filename[256];
     double x[6]; // 求めるパラメータ：x[0]=α、x[1]=β、x[2]=γ、x[3]=cx、x[4]=cy、x[5]=cz
-    double eta = 0.01; // 学習係数
-    int iterate = 100; // 反復回数
+    double eta = 0.0001; // 学習係数
     double epsilon = 0.00001;
     int i;
 
@@ -33,12 +34,14 @@ int main(void)
     readcoeff(filename, coef_l);   // 射影変換の係数の読み込み
     sprintf(filename, "coefficient/coefficient_c.txt");
     readcoeff(filename, coef_c);   // 射影変換の係数の読み込み
+    //sprintf(filename, "coordinate/world_coordinate_c.txt");
+    //readcoordinate(filename, coordinates);   // 3次元座標の読み込み
 
     // 初期値：適宜調整必須
     x[0] = 0; // α
     x[1] = 0; // β
     x[2] = 0; // γ
-    x[3] = 0; // cx
+    x[3] = 10; // cx
     x[4] = 0; // cy
     x[5] = 0; // cz
 
@@ -48,17 +51,30 @@ int main(void)
     printf("計算開始\n");
 
     i = 1;
-    while (fabs(dfpa(x)) > epsilon && fabs(dfpb(x)) > epsilon && fabs(dfpg(x)) > epsilon && fabs(dfpcx(x)) > epsilon && fabs(dfpcy(x)) > epsilon && fabs(dfpcz(x)) > epsilon)
+    while (1)
     {
-         // ベクトルxの更新
-        update_x(x, eta);
-        i++;
+        if(fabs(dfpa(x)) < epsilon && fabs(dfpb(x)) < epsilon && fabs(dfpg(x)) < epsilon && fabs(dfpcx(x)) < epsilon && fabs(dfpcy(x)) < epsilon && fabs(dfpcz(x)) < epsilon)
+        {
+            break;
+        }
+        else
+        {
+             // ベクトルxの更新
+            update_x(x, eta);
+            i++;
+        }  
     }
 
     printf("試行回数：%d\n", i);
     printf("実行結果\n");
     printf("α = %f\nβ = %f\nγ = %f\ncx = %f\ncy = %f\ncz = %f\n", x[0], x[1], x[2], x[3], x[4], x[5]);
     printf("f(x) = %f\n", fp(x));
+    printf("dfpa(x) = %f\n", fabs(dfpa(x)));
+    printf("dfpb(x) = %f\n", fabs(dfpb(x)));
+    printf("dfpg(x) = %f\n", fabs(dfpg(x)));
+    printf("dfpcx(x) = %f\n", fabs(dfpcx(x)));
+    printf("dfpcy(x) = %f\n", fabs(dfpcy(x)));
+    printf("dfpcz(x) = %f\n", fabs(dfpcz(x)));
     printf("\n計算終了\n");
 
     return 0;
@@ -89,20 +105,42 @@ void readcoeff(char filename[], double coef[])
     fclose(fp);
 }
 
+void readcoordinate(char filename[], double coordinate[])
+{
+    FILE *fp;
+    char buffer[256];
+    int num = 210;
+    int i;
+
+    if ((fp = fopen(filename, "r")) == NULL)
+    {
+        printf("%sを開けません\n", filename);
+        exit(1);
+    }
+    else
+    {
+        for (i = 0; i < num; i++)
+        {
+            if (fgets(buffer, 256, fp) != NULL)
+            {
+                coordinate[i] = atof(buffer);
+            }
+        }
+    }
+    fclose(fp);
+}
+
 // 左と中央の重ね合わせ
 double fp(double x[])
 {
     //x[0]=alpha、x[1]=beta、x[2]=gamma、x[3]=cx、x[4]=cy、x[5]=cz
     double p;
-    double x_w, y_w, z_w, x_, y_, z_;
+    double x_, y_, z_;
     double s, t, u, v;
     double rad_a, rad_b, rad_g;
     double a, b, c, d, e, f, g, h, i; // 回転行列の各要素
-
-    // 3次元座標
-    x_w = 30;
-    y_w = 180;
-    z_w = 430;
+    int num = 4;
+    int j, index;
 
     /* ラジアンに変換 */
     rad_a = x[0] * PI / 180.0;
@@ -120,19 +158,23 @@ double fp(double x[])
     h = sin(rad_a)*cos(rad_g)+(cos(rad_a)*sin(rad_b)*sin(rad_g));
     i = cos(rad_a)*cos(rad_b);
 
-    // 左カメラの3軸を回転、平行移動
-    x_ = a*x_w + b*y_w + c*z_w - x[3];
-    y_ = d*x_w + e*y_w + f*z_w - x[4]; 
-    z_ = g*x_w + h*y_w + i*z_w - x[5];
+    p = 0;
+    for (j = 0, index = 0; j < num; j++, index += 3)
+    {
+        // 左カメラの3軸を回転、平行移動
+        x_ = a*coordinates[index] + b*coordinates[index + 1] + c*coordinates[index + 2] - x[3];
+        y_ = d*coordinates[index] + e*coordinates[index + 1] + f*coordinates[index + 2] - x[4]; 
+        z_ = g*coordinates[index] + h*coordinates[index + 1] + i*coordinates[index + 2] - x[5];
 
-    // 画像上での座標を取得
-    u = (coef_l[0]*x_ + coef_l[1]*y_ + coef_l[2]*z_ + coef_l[3]) / (coef_l[8]*x_ + coef_l[9]*y_ + coef_l[10]*z_ + 1);
-    v = (coef_l[4]*x_ + coef_l[5]*y_ + coef_l[6]*z_ + coef_l[7]) / (coef_l[8]*x_ + coef_l[9]*y_ + coef_l[10]*z_ + 1);
+        // 画像上での座標を取得
+        s = (coef_c[0]*coordinates[index] + coef_c[1]*coordinates[index + 1] + coef_c[2]*coordinates[index + 2] + coef_c[3]) / (coef_c[8]*coordinates[index] + coef_c[9]*coordinates[index + 1] + coef_c[10]*coordinates[index + 2] + 1);
+        t = (coef_c[4]*coordinates[index] + coef_c[5]*coordinates[index + 1] + coef_c[6]*coordinates[index + 2] + coef_c[7]) / (coef_c[8]*coordinates[index] + coef_c[9]*coordinates[index + 1] + coef_c[10]*coordinates[index + 2] + 1);
 
-    s = (coef_c[0]*x_w + coef_c[1]*y_w + coef_c[2]*z_w + coef_c[3]) / (coef_c[8]*x_w + coef_c[9]*y_w + coef_c[10]*z_w + 1);
-    t = (coef_c[4]*x_w + coef_c[5]*y_w + coef_c[6]*z_w + coef_c[7]) / (coef_c[8]*x_w + coef_c[9]*y_w + coef_c[10]*z_w + 1);
+        u = (coef_l[0]*x_ + coef_l[1]*y_ + coef_l[2]*z_ + coef_l[3]) / (coef_l[8]*x_ + coef_l[9]*y_ + coef_l[10]*z_ + 1);
+        v = (coef_l[4]*x_ + coef_l[5]*y_ + coef_l[6]*z_ + coef_l[7]) / (coef_l[8]*x_ + coef_l[9]*y_ + coef_l[10]*z_ + 1);
 
-    p = fabs(s - u) + fabs(t - v);
+        p += (s - u)*(s - u) + (t - v)*(t - v);
+    }
 
     return p;
 }
